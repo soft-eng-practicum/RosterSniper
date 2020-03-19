e8279fd2df6f787c5544ba3acd39ea88edeee386
Ryan Cosentino <rjc0514@gmail.com>
2020-03-19 02:00:04 -0400

    Added autologin after register

--- roster_sniper/users/views.py
+++ roster_sniper/users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+
 from .forms import MyUserCreationForm, UserUpdateForm, ProfileUpdateForm
 
 
@@ -10,11 +12,15 @@ def register(request):
         form = MyUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('login')
+            messages.info(request, "Thanks for registering. You are now logged in.")
+            new_user = authenticate(username=form.cleaned_data['username'],
+                                    password=form.cleaned_data['password1'])
+
+            login(request, new_user)
+            return redirect('courses')
     else:
         form = MyUserCreationForm()
+
     return render(request, 'register.html', {'form': form})
 
 
