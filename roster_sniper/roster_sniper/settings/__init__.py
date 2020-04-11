743838d1c43181510d5702837491f9264215d173
Shaun Mitchell <shaun@shitchell.com>
2020-04-11 04:33:19 +0000

    default to production mode

--- roster_sniper/roster_sniper/settings/__init__.py
+++ roster_sniper/roster_sniper/settings/__init__.py
@@ -4,7 +4,7 @@ Import settings based on the DJANGO_ENVIRONMENT variable
 
 import os
 
-if os.environ.get('DJANGO_ENVIRONMENT') == 'production':
-    from .production import *
-else:
+if os.environ.get('DJANGO_ENVIRONMENT') == 'development':
     from .development import *
+else:
+    from .production import *
