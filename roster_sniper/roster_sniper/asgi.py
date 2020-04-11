f002cb5c01d8f9d050694dfa2682f2cfc390970d
Shaun Mitchell <shaun@shitchell.com>
2020-04-11 03:18:42 +0000

    adjusted settings to easily switch between development or production modes

--- roster_sniper/roster_sniper/asgi.py
+++ roster_sniper/roster_sniper/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'roster_sniper.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'roster_sniper.settings.' + os.environ.get('DJANGO_ENVIRONMENT', 'production'))
 
 application = get_asgi_application()
