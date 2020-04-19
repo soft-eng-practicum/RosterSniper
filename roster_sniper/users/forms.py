9033248e4ad3d447e008fd273e66d598f1ccde52
Shaun Mitchell <shaun@shitchell.com>
2020-04-19 20:02:19 +0000

    added first name to registration and profile pages

--- roster_sniper/users/forms.py
+++ roster_sniper/users/forms.py
@@ -9,7 +9,7 @@ class MyUserCreationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'email', 'password1', 'password2']
 
 
 class UserUpdateForm(forms.ModelForm):
@@ -17,7 +17,7 @@ class UserUpdateForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['email']
+        fields = ['email', 'first_name']
 
 
 class ProfileUpdateForm(forms.ModelForm):
