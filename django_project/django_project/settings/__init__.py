07cbb5b7a07e896ec05a904fa6025aa09835e72b
Ryan Cosentino <rjc0514@gmail.com>
2020-08-22 13:14:15 -0400

    Moved environment variable settings to local.py

--- django_project/django_project/settings/__init__.py
+++ django_project/django_project/settings/__init__.py
@@ -1,17 +1,36 @@
-"""
-Import settings based on the DJANGO_ENVIRONMENT variable
-"""
+'''
+Settings are imported as follows:
+    __init__ -> local (optional) -> development/production -> base
 
-# Explanation: https://stackoverflow.com/a/15325966
+This lets the local settings file modify settings specified in the development,
+production, or base settings file. It also lets the development or production
+settings files modify settings specified in the base settings file. For example,
+development.py can include
+
+>>> INSTALLED_APPS += 'some_debug_app'
+
+because the INSTALLED_APPS list is defined in base.py. In addition to the
+previous statement, local.py can also include
+
+>>> DATABASES['default']['PASSWORD'] = 'some_password'
+
+because the DATABASES dictionary is defined in development.py/production.py.
+
+For more information about Django settings, check out the following links
+General information:   https://docs.djangoproject.com/en/3.1/topics/settings/
+List of all settings:  https://docs.djangoproject.com/en/3.1/ref/settings/
+'''
 
 import os
 
-if os.environ.get('RS_ENVIRONMENT') == 'production':
-    from .production import *
-else:
-    from .development import *
+
 
 try:
     from .local import *
-except ImportError as e:
-    pass
+
+# Defaults to development mode if local.py and environment variable don't exist
+except ImportError:
+	if os.environ.get('RS_ENVIRONMENT') == 'production':
+    	from .production import *
+    else
+    	from .development import *
--- django_project/django_project/settings/local_example.py
+++ django_project/django_project/settings/local_example.py
@@ -1,18 +1,47 @@
 '''
-If you want to use local settings, copy this file and rename it to 'local.py',
-then change the settings as you like. The renamed file is in the gitignore but
-you should always make sure you aren't committing your passwords!
+The local settings file is meant to include passwords or any other setting which
+for whatever reason shouldn't be tracked by git. DO NOT specify them here! You
+MUST copy this file, rename it to 'local.py', and then change the settings as
+you like. The renamed file is in the gitignore and wont be tracked.
+
+The following can be used to generate a new SECRET_KEY
+>>> python -c 'from django.core.management import utils; print(utils.get_random_secret_key())'
+cm+6y_4_jd^ri3kx##spu3axn*)d-=a6d)ej)@1wwh%z1kj)g@
 '''
+
+DEBUG = True
+
+if DEBUG:
+    from .development import *
+else:
+    from .production import *
+
+
+## General local settings example ##############################################
 
 # List of people who get error email notifications
 # Django only sends them when DEBUG=False however
 # core.utils.send_admin_email() will send regardless of DEBUG status
-# ADMINS = [('YourName', 'YourEmail')]
+# ADMINS = [('your_name', 'your_email')]
 
-# Email
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'email@gmail.com'
-EMAIL_HOST_PASSWORD = 'password'
-EMAIL_PORT = 587
 
-# Custom Database?
+## Development local settings example ##########################################
+
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'example@gmail.com'
+# EMAIL_HOST_PASSWORD = 'some_password'
+# EMAIL_PORT = 587
+
+# Custom database or logger..
+
+
+## Production local settings example ###########################################
+
+'''
+The following can be used to generate a new SECRET_KEY
+>>> python -c 'from django.core.management import utils; print(utils.get_random_secret_key())'
+cm+6y_4_jd^ri3kx##spu3axn*)d-=a6d)ej)@1wwh%z1kj)g@
+'''
+# SECRET_KEY = 'a_secret_key'
+
+# DATABASES['default']['PASSWORD'] = 'some_password'
