a958292cd951acd0dafbd0fbf3036ab10495105a
Shaun Mitchell <shaun@shitchell.com>
2020-09-01 22:13:25 +0000

    updated gunicorn script

--- django_project/start_gunicorn.sh
+++ django_project/start_gunicorn.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sock_path="/var/www/rostersniper.com/roster_sniper.sock"
+sock_path="roster_sniper.sock"
+if [ -n "$1" ]; then
+    sock_path="$1"
+fi
 wsgi_path="roster_sniper.wsgi:application"
 
 gunicorn --access-logfile - --workers 3 --bind unix:$sock_path $wsgi_path
