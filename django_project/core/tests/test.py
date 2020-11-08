4f8324c3f2fa57f42dd0e517e6f070e657ea6050
Merchant151 <jmiller32@ggc.edu>
2020-11-07 23:16:07 -0500

    created tests the Term class in courses.py

--- django_project/core/tests/test.py
+++ django_project/core/tests/test.py
@@ -2,10 +2,10 @@ import sys
 import os.path
 sys.path.append("./core/models")
 
-print(os.path.isfile("./core/models/schools.py"))
-print(os.path.isfile("./manage.py"))
-print(os.getcwd())
-print(sys.path)
+#print(os.path.isfile("./core/models/schools.py"))
+#print(os.path.isfile("./manage.py"))
+#print(os.getcwd())
+#print(sys.path)
 
 print("This is the Unit Test file")
 from django.test import TestCase
@@ -25,6 +25,7 @@ from core.utils import full_reverse
 from ..models import courses
 
 class BasicTest(TestCase):
+	term = None
 	#Test Functions need to begin with test_
 	#This test is to test courses feilds  
 	def test_fields(self):
@@ -37,3 +38,18 @@ class BasicTest(TestCase):
 		
 		record = courses.Professor.objects.get(pk=1)#email ="test@test.test"
 		self.assertEqual(record, professor)
+
+		
+	def setUp(self):
+		self.term = courses.Term()
+		self.term.code = "202008"
+		self.term.description = "This is a dummy discription"
+		self.term.save()
+
+	def test_term_feilds(self):
+		record = courses.Term.objects.get(pk = self.term.code)
+		self.assertEqual(self.term,record)
+
+	def test_term_GetTerms(self):
+		self.assertEqual(self.term.code, "202008")
+		self.assertTrue(self.term.update)
