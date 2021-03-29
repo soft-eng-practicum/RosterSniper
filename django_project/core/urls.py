295061a1a3441806b069b38d2e8c17dcfc50ab81
Ryan Cosentino <rjc0514@gmail.com>
2021-03-29 18:10:40 -0400

    Added API (no DRF)
    For School, Professor, Term, Subject, Course, and Section

--- django_project/core/urls.py
+++ django_project/core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from . import views
+from . import views, views_api
+
 
 urlpatterns = [
 	path('', views.home, name='home'),
@@ -8,4 +9,12 @@ urlpatterns = [
 	path('add-courses/<str:school>/', views.add_courses, name='add_courses'),
 	path('my-courses/', views.my_courses, name='my_courses'),
 	path('unsubscribe/<str:unsub_type>/<uuid:unsub_id>/', views.unsubscribe, name='unsubscribe'),
+
+	# My API
+	path('api/schools/', views_api.schools),
+	path('api/<str:school>/professors/', views_api.professors),
+	path('api/<str:school>/terms/', views_api.terms),
+	path('api/<str:school>/subjects/', views_api.subjects),
+	path('api/<str:school>/courses/', views_api.courses),
+	path('api/<str:school>/sections/', views_api.sections),
 ]
---
+++ django_project/core/views_api.py
@@ -0,0 +1,77 @@
+from django.http import JsonResponse, Http404
+
+from .models import School, Professor, Term, Subject, Course, Section
+
+
+def schools(request):
+	s = School.objects.filter(active=True).values(
+		'name', 'short_name', 'color_hex'
+	)
+	return JsonResponse(list(s), safe=False)
+
+
+def view_maker(queryset):
+
+	def view(request, school):
+		try:
+			s = School.objects.get(short_name=school)
+		except School.DoesNotExist:
+			raise Http404()
+
+		qs = queryset.filter(school=s)
+
+		try:
+			if offset := request.GET.get('offset'):
+				if (offset := int(offset)) >= 0:
+					qs = qs[offset:]
+				else:
+					return JsonResponse('offset must be non-negative', safe=False)
+
+			if limit := request.GET.get('limit'):
+				if (limit := int(limit)) >= 0:
+					qs = qs[:limit]
+				else:
+					return JsonResponse('limit must be non-negative', safe=False)
+
+		except ValueError:
+			return JsonResponse('offset/limit must be non-negative integers', safe=False)
+
+		return JsonResponse(list(qs), safe=False)
+
+	return view
+
+
+professors = view_maker(
+	Professor.objects.values(
+		'email', 'firstname', 'lastname'
+	)
+)
+
+terms = view_maker(
+	Term.objects.filter(display=True).values(
+		'code', 'description',
+	)
+)
+
+subjects = view_maker(
+	Subject.objects.values(
+		'short_title', 'long_title'
+	)
+)
+
+courses = view_maker(
+	Course.objects.values(
+		'subject__short_title', 'number', 'title', 'credit_hours'
+	)
+)
+
+sections = view_maker(
+	Section.objects.values(
+		'course__subject__short_title',
+		'course__number', 'course__title',
+		'term__code', 'crn', 'section_num', 'section_title', 'credit_hours',
+		'professor__firstname', 'professor__lastname',
+		'days', 'start_time', 'end_time', 'room',
+		'enrolled', 'capacity'
+	)
+)
